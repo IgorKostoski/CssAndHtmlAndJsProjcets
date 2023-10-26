8c536f548ddd0bdbcd29140cc8b0469a48795ebc
IgorKostoski <92607214+IgorKostoski@users.noreply.github.com>
2023-10-26 22:51:15 +0200

    f commit

---
+++ 157.ReactAnimatedNavigation/animatednav/src/Components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./nav.css";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+    <h1>Navigation</h1>
+    </div>
+  );
+};
+
+export default Nav;
