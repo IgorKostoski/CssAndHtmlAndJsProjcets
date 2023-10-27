77c9ec58aeec23c54535e1adadf8f3d922ee9903
IgorKostoski <92607214+IgorKostoski@users.noreply.github.com>
2023-10-27 14:00:12 +0200

    navbar commit

--- 157.ReactAnimatedNavigation/animatednav/src/Components/Nav/Nav.js
+++ 157.ReactAnimatedNavigation/animatednav/src/Components/Nav/Nav.js
@@ -1,13 +1,38 @@
 import React from "react";
 
+import { useState } from "react";
+
 import "./nav.css";
 
+
+
 const Nav = () => {
-  return (
-    <div className="nav">
-    <h1>Navigation</h1>
-    </div>
-  );
+
+    const [navOpen, setNavOpen] = useState(false);
+
+
+    return (
+        <div className="nav">
+          <div className="nav-container">
+            <div className="navbar">
+              <div className="logo">Design Style</div>
+              <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
+                <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
+                  <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
+                  <span className={navOpen ? "lineBottom spin" : "lineBottom"}></span>
+                </div>
+              </div>
+            </div>
+            <div
+              className="nav-overlay"
+              style={{
+                top: navOpen ? "0" : "-100%", // Fixed the syntax here
+                transitionDelay: navOpen ? "0s" : "0s", // Fixed the syntax here
+              }}
+            ></div>
+          </div>
+        </div>
+      );
 };
 
 export default Nav;
