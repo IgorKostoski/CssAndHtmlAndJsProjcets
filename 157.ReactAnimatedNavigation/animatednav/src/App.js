0955c7ba6091d183456f3f7650b917c184503c56
IgorKostoski <92607214+IgorKostoski@users.noreply.github.com>
2023-10-27 19:52:41 +0200

    navbar second commit

--- 157.ReactAnimatedNavigation/animatednav/src/App.js
+++ 157.ReactAnimatedNavigation/animatednav/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Nav from "./Components/Nav/Nav"
 
+
+
+
+
+
 const App = () => {
     return ( <div className="App">
         <Nav />
     </div>
+
+
+
     );
 };
+
+
 
 
 export default App;
--- 157.ReactAnimatedNavigation/animatednav/src/Components/Nav/Nav.js
+++ 157.ReactAnimatedNavigation/animatednav/src/Components/Nav/Nav.js
@@ -2,37 +2,97 @@ import React from "react";
 
 import { useState } from "react";
 
+import { Link } from "react-router-dom";
+
 import "./nav.css";
 
-
-
 const Nav = () => {
+  const [navOpen, setNavOpen] = useState(false);
 
-    const [navOpen, setNavOpen] = useState(false);
-
-
-    return (
-        <div className="nav">
-          <div className="nav-container">
-            <div className="navbar">
-              <div className="logo">Design Style</div>
-              <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
-                <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
-                  <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
-                  <span className={navOpen ? "lineBottom spin" : "lineBottom"}></span>
-                </div>
-              </div>
+  return (
+    <div className="nav">
+      <div className="nav-container">
+        <div className="navbar">
+          <div className="logo">Design Style</div>
+          <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
+            <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
+              <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
+              <span
+                className={navOpen ? "lineBottom spin" : "lineBottom"}
+              ></span>
             </div>
-            <div
-              className="nav-overlay"
-              style={{
-                top: navOpen ? "0" : "-100%", // Fixed the syntax here
-                transitionDelay: navOpen ? "0s" : "0s", // Fixed the syntax here
-              }}
-            ></div>
           </div>
         </div>
-      );
+        <div
+          className="nav-overlay"
+          style={{
+            top: navOpen ? "0" : "-100%", // Fixed the syntax here
+            transitionDelay: navOpen ? "0s" : "0s", // Fixed the syntax here
+          }}
+        >
+          <ul className="nav-links">
+            <li className="nav-item">
+              <Link to="/" onClick={() => setNavOpen(!navOpen)}  style={{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen? "0.8s" : "0s",
+              }}>Home</Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+            <li className="nav-item">
+              <Link to="/projects" onClick={() => setNavOpen(!navOpen)}  style={{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen? "0.9s" : "0s",
+              }}>Projects</Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" onClick={() => setNavOpen(!navOpen)}  style={{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen? "1s" : "0s",
+              }}>About</Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+            <li className="nav-item">
+              <Link to="/Contact" onClick={() => setNavOpen(!navOpen)}  style={{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen? "1.1s" : "0s",
+              }}>Contact</Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+          </ul>
+
+          <div className="nav-footer">
+            <div className="location" style={{
+                bottom: navOpen ? "0" : "-20px",
+                opacity: navOpen ? "1" : "0",
+                transitionDelay: navOpen ? "1.2s" : "0s",
+            }}
+            >
+              <span>Cologne, Germany</span>
+            </div>
+            <div className="nav-social-media">
+              <ul>
+                <li>
+                  <a href="Youtube" style={{
+                bottom: navOpen ? "0" : "-20px",
+                opacity: navOpen ? "1" : "0",
+                transitionDelay: navOpen ? "1.3s" : "0s",
+            }}>Youtube</a>
+                </li>
+                <li>
+                  <a href="Instagram" style={{
+                bottom: navOpen ? "0" : "-20px",
+                opacity: navOpen ? "1" : "0",
+                transitionDelay: navOpen ? "1.4s" : "0s",
+            }}>Instagram</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
